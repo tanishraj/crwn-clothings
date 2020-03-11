858185b06f3996ce403fb787519ad8957d922620
Tanish Raj <Tanish.Raj@shell.com>
2020-03-11 16:22:30 +0530

    Routing has been implemented.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/home-page.component';
 
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+    </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/hats" component={HatsPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
