c273aebd009471b9e3da8b33cf95a176202adb12
Tanish Raj <Tanish.Raj@shell.com>
2020-03-11 17:10:42 +0530

    Shop page has been added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/home-page.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-    </div>
-)
 
 function App() {
   return (
@@ -16,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/hats" component={HatsPage} />
+          <Route path="/shop" component={ShopPage} />
         </Switch>
       </BrowserRouter>
     </div>
