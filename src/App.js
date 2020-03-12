400219ad939b510d441dc72ca8275727f40549fb
Tanish Raj <Tanish.Raj@shell.com>
2020-03-12 18:55:42 +0530

    API

--- src/App.js
+++ src/App.js
@@ -4,22 +4,38 @@ import HomePage from './pages/homepage/home-page.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header-component';
 import SignInAndSignupPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/signinandsignup" component={SignInAndSignupPage} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route path="/signinandsignup" component={SignInAndSignupPage} />
+          </Switch>
+        </BrowserRouter>
+      </div >
+    );
+  }
 }
 
 export default App;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+import {signInWithGoogle} from '../../firebase/firebase.utils';
 
 import "./sign-in.style.scss";
 
@@ -50,6 +51,7 @@ class SignIn extends React.Component {
           />
 
           <CustomButton type="submit">SIGN IN</CustomButton>
+          <CustomButton onClick={signInWithGoogle}>SIGN WITH GOOGLE</CustomButton>
         </form>
       </div>
     );
