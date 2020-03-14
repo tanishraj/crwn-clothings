5011df937b9a5345a69b72dae3deb6111782cf6e
tanishraj <tanishraj.quitegrowth@gmail.com>
2020-03-14 14:38:00 +0530

    Sign in and singup page inncluding all the functionalities

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,31 @@ const config = {
 	messagingSenderId: "513984954251",
 	appId: "1:513984954251:web:8bdbf55f916d9eb8d6f98e",
 	measurementId: "G-JNYEWGGDML"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+	const snapShot = await userRef.get();
+
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			})
+		} catch (error) {
+			console.log('error creating user', error.message);
+		}
+	}
+
+	return userRef;
 }
 
 firebase.initializeApp(config);
